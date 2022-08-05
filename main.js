d502f2cb3f24b5ccadef8dd31fa5d09b9e649c30
Vishal Sharma <33948196+vishal0021@users.noreply.github.com>
2022-08-05 14:25:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//capture the yesterday's image of the day
+//take image from the existing database of images
+//Beg C1
+var image;
+var date = "Fri Aug 05 2022 13:44:51 GMT+0530" //local time
+image = getImage(date)
+//add image in the list of stages
+
+switch_background(image) //tynker block
